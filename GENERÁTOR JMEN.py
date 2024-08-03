c60e3b4316a188eedf9ce774bf0404144a5264c3
NNW Developer <matej.truxa@gmail.com>
2024-08-03 20:35:17 +0200

    Add files via upload

---
+++ GENERÁTOR JMEN.py
@@ -0,0 +1,12 @@
+print ("Ahoj, já jsem magický generátor vtipných jmen.")
+print ("Jaké je tvé křestní jméno?")
+km = input()
+
+veta1 = ("Dobře, ahoj")
+veta2 = ("Teď napiš svojí vlastnost (např.: Kamarádský):")
+
+print (veta1, km,veta2)
+prijmeni = input()
+
+print ("Super, tvé nové jméno je:")
+print (km, prijmeni)
